4d409e251b5ed6f8ae34b1a5e1e54b89eaf2aae4
Philip Lewis <philipjonlewis@gmail.com>
2022-03-22 21:12:52 +0800

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+import * as React from "react";
+import * as Server from "react-dom/server";
+
+let Greet = () => <h1>Hello, world!</h1>;
+console.log(Server.renderToString(<Greet />));
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello ES BUild!</h1>
+  <h5> THis is the esbuild bundler thing</h5>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
