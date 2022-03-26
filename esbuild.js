7677d4b96cec278d0edc6c4bfd0f442bb67e56da
Philip Lewis <philipjonlewis@gmail.com>
2022-03-26 23:26:56 +0800

    fixing esbuild config

--- esbuild.js
+++ esbuild.js
@@ -3,17 +3,33 @@ import { sassPlugin } from "esbuild-sass-plugin";
 import htmlPlugin from "@chialab/esbuild-plugin-html";
 
 await esbuild
-  .build({
-    entryPoints: [
-      "src/index.html",
-      "src/about.html",
-      "src/styles/style.scss",
-      "src/scripts/app.jsx",
-    ],
-    outdir: "docs",
-    bundle: true,
-    minify: true,
-    plugins: [sassPlugin(), htmlPlugin()],
-  })
+  .build(
+    {
+      entryPoints: ["src/index.html"],
+      outdir: "docs",
+      bundle: true,
+      watch: {
+        onRebuild(error, result) {
+          if (error) console.error("watch build failed:", error);
+          else console.log("watch build succeeded:");
+        },
+      },
+      minify: true,
+      format: "esm",
+      splitting: true,
+      sourcemap: true,
+      loader: {
+        ".png": "dataurl",
+        ".svg": "text",
+      },
+      target: ["chrome58", "firefox57", "safari11", "edge16"],
+      plugins: [sassPlugin(), htmlPlugin()],
+    },
+    {
+      loader: "css",
+      minify: true,
+    }
+  )
+
   .then(() => console.log("⚡ Build complete! ⚡"))
   .catch(() => process.exit(1));
--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,12 +1,14 @@
-import "../styles/style.scss";
 import * as React from "react";
 import * as Server from "react-dom/server";
 
-let Greet = () => <h1>Hello, world!</h1>;
+let Greet = () => "<h1>Hello, world!</h1>";
 console.log(Server.renderToString(<Greet />));
 
 document.querySelector("#app").innerHTML = `
   <h1>Hello ES BUild!</h1>
+  <h1>Hello ES BUild!</h1>
+  <h1>ESBUILD Number 1</h1>
+  <h1>ESBUILD Number 1</h1>
   <h5> THis is the esbuild bundler thing</h5>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
